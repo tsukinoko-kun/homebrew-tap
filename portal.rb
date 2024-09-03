d51aa8f0b06ce8b0c8c0f5b15d147471ecb639e3
tsukinoko-kun <gh@frankmayer.dev>
2024-09-03 17:45:38 +0200

    Brew formula update for portal version v1.1.0

--- portal.rb
+++ portal.rb
@@ -5,20 +5,20 @@
 class Portal < Formula
   desc "Simple local file sharing"
   homepage "https://github.com/tsukinoko-kun/portal"
-  version "1.0.0"
+  version "1.1.0"
 
   on_macos do
     on_intel do
-      url "https://github.com/tsukinoko-kun/portal/releases/download/v1.0.0/portal_1.0.0_darwin_amd64.tar.gz"
-      sha256 "a469c2af171a39a3ac56e7b63716f15b79e9217d2b3ab748e6a0e031fce641e9"
+      url "https://github.com/tsukinoko-kun/portal/releases/download/v1.1.0/portal_1.1.0_darwin_amd64.tar.gz"
+      sha256 "f24fbd20f1cf375e62555ad2f92b818be8e27004f8f59426dd50ca77d42467cc"
 
       def install
         bin.install "portal"
       end
     end
     on_arm do
-      url "https://github.com/tsukinoko-kun/portal/releases/download/v1.0.0/portal_1.0.0_darwin_arm64.tar.gz"
-      sha256 "9b912be7ec21608e3963f4bd4da8a32746dc79ea8ab5fb63353d549e231a126c"
+      url "https://github.com/tsukinoko-kun/portal/releases/download/v1.1.0/portal_1.1.0_darwin_arm64.tar.gz"
+      sha256 "0c97c5c7f841d38ec23e1c883fa88fda94fb7e48573f344cb119ff96c8046bbd"
 
       def install
         bin.install "portal"
@@ -29,8 +29,8 @@ class Portal < Formula
   on_linux do
     on_intel do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/tsukinoko-kun/portal/releases/download/v1.0.0/portal_1.0.0_linux_amd64.tar.gz"
-        sha256 "bc5c496652a4601feeab8df89f4c8af92d73f18a892a83ed7e2959a4234ece54"
+        url "https://github.com/tsukinoko-kun/portal/releases/download/v1.1.0/portal_1.1.0_linux_amd64.tar.gz"
+        sha256 "e11f65740df69c9b9a36369faad28b5a756c11508ada7f4ef9dafcbed6edb9dd"
 
         def install
           bin.install "portal"
@@ -39,8 +39,8 @@ class Portal < Formula
     end
     on_arm do
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/tsukinoko-kun/portal/releases/download/v1.0.0/portal_1.0.0_linux_arm64.tar.gz"
-        sha256 "702cf1bbd04c76081911ac4fe845c77e19c6e7bd91a2093d4ba1d7c11e38f96c"
+        url "https://github.com/tsukinoko-kun/portal/releases/download/v1.1.0/portal_1.1.0_linux_arm64.tar.gz"
+        sha256 "25518aa7699c78675650edb7716fad4e70d64b1771f090c9198eb76372989cb5"
 
         def install
           bin.install "portal"
